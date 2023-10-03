7dcd2d58b8c3c884e1b09a1d4216b98efebc630c
Fran Ruiz <fjavierrule@gmail.com>
2023-10-03 14:30:55 +0200

    tipos

--- principal.js
+++ principal.js
@@ -1,17 +1,52 @@
 $(document).ready(function () {
 
+    // $.ajax({
+    //     url: 'https://pokeapi.co/api/v2/pokemon/',
+    //     type: 'GET'
+    // }).done(function (resp) {
+    //     var listadoPokemons = resp.results;
+    //     var idPk = 1;
+    //     var color = "";
+
     $.ajax({
-        url: 'https://pokeapi.co/api/v2/pokemon/',
+        url: 'https://pokeapi.co/api/v2/type/',
         type: 'GET'
-    }).done(function (resp) {
-        var listadoPokemons = resp.results;
-        var idPk = 1;
+    }).done(function (tipo) {
+        var listadoTipos = tipo.results;
+
+        listadoTipos.forEach(function (types) {
+            console.log(types.name)
+
+            // if (types.name == 'grass') {
+            //     $('.card-body').addClass('.grass');
+            // }
+
+            $.ajax({
+                url: 'https://pokeapi.co/api/v2/pokemon/',
+                type: 'GET'
+            }).done(function (resp) {
+                var listadoPokemons = resp.results;
+                var idPk = 1;
+                var color = "";
+
+
+            });
+        });
+
         listadoPokemons.forEach(function (pokemon) {
+
+            console.log(pokemon)
+
+            // var tipo = pokemon.types[0].type.name;
+
+            // console.log(tipo)
+
+
 
             var template = `<div class="card m-5 border-4 border-warning" id="carta-sombra" style="width: 18rem;">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${idPk}.png"
                 class="card-img-top" alt="..." height="240px">
-            <div class="card-body" id="card-electric">
+            <div class="card-body" id="${types.name}">
                 <div class="miniDiv">
                     <p class="text-center pt-2">${pokemon.name}</p>
                 </div>
@@ -23,14 +58,11 @@ $(document).ready(function () {
         </div>`;
             $('#lista-pokemon').append(template);
             idPk++;
+
+
         });
     });
 
 
-
-    function getPokemonIdFromUrl(urlType) {
-        // Sacar id de la url de pokemon
-        return url.split('/').reverse()[1];
-    }
 
 });
