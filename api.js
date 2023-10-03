86da427967067e00e20c4c6c55854276e95fd86e
Fran Ruiz <fjavierrule@gmail.com>
2023-10-03 19:58:45 +0200

    solucionar el boton

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
-
-const numPokemons = 100;
+const numPokemons = 20;
 const pokemons = [];
 
 $(document).ready(function () { 
@@ -12,27 +11,36 @@ $(document).ready(function () {
         }).done(function (resp) {
             pokemons.push(resp);
 
-            var template = `<div class="card m-5 border-4 border-warning" id="carta-sombra" style="width: 18rem;">
+            var template = `<div class="card m-5 border-4 border-warning ${resp.types[0].type.name} carta-sombra">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${resp.id}.png"
                 class="card-img-top" alt="..." height="240px">
-            <div class="card-body" id="${resp.types[0].type.name}">
+            <div class="card-body">
                 <div class="miniDiv">
                     <p class="text-center pt-2">${resp.name}</p>
+                    <button type="button" class="btn-view-pokemon">prueba</button>
                 </div>
-                <p class="card-text pt-4">Ataque: ${resp.stats[1].base_stat}
-                    <hr>
-                    Habilidad: ${resp.abilities[0].ability.name}
-                </p>
+               
             </div>
         </div>`;
             $('#lista-pokemon').append(template);
             
         });
 
+        // IMPORTANTE: IDENTIFICAR QUE DIVS ES EL QUE SE HA PULSADO Y SACAR SU HTML AL CLICKAR EL BOTON
+
+       
     }
+  
 
     //To Do: ordenar el array para que los pokemons salgan en orden
 
     //console.log(pokemons);
 
+//     <p class="card-text pt-4">Ataque: ${resp.stats[1].base_stat}
+//     <hr>
+//     Habilidad: ${resp.abilities[0].ability.name}
+// </p>
+
 });
+
+
