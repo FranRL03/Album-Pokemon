5545307dfda9e063e736e78c9409210d824800c3
Pedro <pedrofe.av@gmail.com>
2023-10-03 18:21:14 +0200

    pintar items js

---
+++ item.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    console.log('Hola')
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/item?limit=20',
+        type: 'GET'
+    }).done(function (resp) {
+        var listaItems = resp.results;
+        listaItems.forEach(function (item) {
+            $.ajax({
+                url: item.url,
+                type: 'GET'
+            }).done(function (objeto) {
+                var template = `<span class="badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src=${objeto.sprites.default}><p>${objeto.name}</p></span>`;
+                
+                $('#lista-items').append(template);
+            })
+
+        });
+    });
+
+});
