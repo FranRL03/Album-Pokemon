883319705718a77509e99ba82c1037c3c57a24cc
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-03 12:39:41 +0200

    items js

---
+++ items.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/item?limit=1000',
+        type: 'GET'
+    }).done(function (resp) {
+        var listaItems = resp.results;
+        listaItems.forEach(function (item) {
+
+
+        });
+    });
+
+});
