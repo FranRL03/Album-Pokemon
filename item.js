a7b856046b54f35a07769dac0ee9de3d9a46ce59
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-04 14:15:21 +0200

    modal

--- item.js
+++ item.js
@@ -10,12 +10,30 @@ $(document).ready(function () {
                 url: item.url,
                 type: 'GET'
             }).done(function (objeto) {
-               var template = `<span class="badge rounded-pill col-2 mb-4 ms-5 mt-5" data-bs-toggle="modal" data-bs-target="#myModal"><img src=${objeto.sprites.default}> <p>${objeto.name}</p></span>`;               
-                
+                var template = `<div itemid="${objeto.id}" class="btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src=${objeto.sprites.default}> <p>${objeto.name}</p></div>`;
+
                 $('#lista-items').append(template);
             })
 
         });
     });
+
+    $(document).on('click', '.btn-detail', function () {
+        var itemId = $(this).attr('itemid');
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/item/${itemId}`,
+            type: 'GET'
+        }).done(function (item) {
+
+            $('#item-name').html(item.name);
+            $('#imgItem').attr('src', item.sprites.default);
+            $('#descItem').html(item.flavor_text_entries[0].text);
+            console.log(item.flavor_text_entries.text)
+            $('#itemModal').modal('show');
+
+        })
+    })
+
+
 
 });
