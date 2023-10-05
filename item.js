a44e292a68acd37fc6717425ebdab216112f043a
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-05 12:45:55 +0200

    pagination

--- item.js
+++ item.js
@@ -1,21 +1,29 @@
 $(document).ready(function () {
-    console.log('Hola')
+
     $.ajax({
-        url: 'https://pokeapi.co/api/v2/item?limit=40',
+        url: 'https://pokeapi.co/api/v2/item?limit=20',
         type: 'GET'
     }).done(function (resp) {
+        var totalItems = resp.count;
         var listaItems = resp.results;
+
         listaItems.forEach(function (item) {
             $.ajax({
                 url: item.url,
                 type: 'GET'
             }).done(function (objeto) {
-                var template = `<div itemid="${objeto.id}" class="btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src=${objeto.sprites.default}> <p>${objeto.name}</p></div>`;
-
+                var template = `<div itemid="${objeto.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src=${objeto.sprites.default}> <p>${objeto.name}</p></div>`;
                 $('#lista-items').append(template);
             })
 
         });
+
+        for (var i = 1; i < totalItems / 20; i++) {
+            var template = `<li class="page-item" idPag="${i}"><a class="page-link" href="#">${i}</a></li>`;
+            $('#pagination').append(template);
+        }
+
+
     });
 
     $(document).on('click', '.btn-detail', function () {
