255ef071b248437b0e9ea74110b8039023432a31
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-06 10:01:42 +0200

    paginacion arreglada

--- item.js
+++ item.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $.ajax({
-        url: 'https://pokeapi.co/api/v2/item?limit=100',
+        url: 'https://pokeapi.co/api/v2/item?limit=20&offset=0',
         type: 'GET'
     }).done(function (resp) {
         var totalItems = resp.count;
@@ -22,32 +22,12 @@ $(document).ready(function () {
             var template = `<li class="page-item item" idPag="${i}"><spam class="page-link" href="#">${i}</spam></li>`;
             $('#pagination').append(template);
         };
-
-
-
-
     });
 
-    $(document).on('click', '.item', function () {        
-        var pagina = $(this).attr('idPag');        
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/item?limit=20$offset=${pagina - 1 * 20}`,
-            type: 'GET'
-        }).done(function (itemPag) {            
-            var itemsPag = itemPag.results;
-            borrar();
-            itemsPag.forEach(function(objeto){
-                $.ajax({
-                    url: objeto.url,
-                    type: 'GET'
-                }).done(function(item){                    
-                    var template = `<div itemid="${item.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src=${item.sprites.default}> <p>${item.name}</p></div>`;
-                    $('#lista-items').append(template);
-                });
-                
-            });
-            
-        });
+
+    $(document).on('click', '.item', function () {
+        var pagina = $(this).attr('idPag');
+        getItemsWithOffset((pagina - 1) * 20);
     });
 
     $(document).on('click', '.btn-detail', function () {
@@ -59,17 +39,36 @@ $(document).ready(function () {
 
             $('#item-name').html(item.name);
             $('#imgItem').attr('src', item.sprites.default);
-            $('#descItem').html(item.flavor_text_entries[0]);
+            $('#descItem').html((item.flavor_text_entries[0].text).toUpperCase());
             console.log(item.flavor_text_entries.text)
             $('#itemModal').modal('show');
 
         })
     });
 
+    //Metodo para obtener los pokemons paginados utilizando el offset
+    function getItemsWithOffset(offset) {
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/item?limit=20&offset=${offset}`,
+            type: 'GET'
+        }).done(function (itemPag) {
+            var itemsPag = itemPag.results;
+            borrar();
+            itemsPag.forEach(function (objeto) {
+                $.ajax({
+                    url: objeto.url,
+                    type: 'GET'
+                }).done(function (item) {
+                    var template = `<div itemid="${item.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src=${item.sprites.default}> <p>${item.name}</p></div>`;
+                    $('#lista-items').append(template);
+                });
 
+            });
 
+        });
+    }
 });
 
-function borrar(){
+function borrar() {
     $("#lista-items").empty();
 }
