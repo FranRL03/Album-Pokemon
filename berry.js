acbc013019ed9990342557c882c62be0961d362f
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-07 20:28:40 +0200

    paginacion berries

--- berry.js
+++ berry.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $.ajax({
-        url: 'https://pokeapi.co/api/v2/berry?',
+        url: 'https://pokeapi.co/api/v2/berry?limit=20&offset=0',
         type: 'GET'
     }).done(async function(respuesta) {
         var totalBerries = respuesta.count;
         var listaBerries = respuesta.results;
-        var i = 1;
+        
     
         for (const item of listaBerries) {
             try {
@@ -14,22 +14,66 @@ $(document).ready(function(){
                     type: 'GET'
                 });
                 
-                if (i < 9) {
-                    var template = `<div itemid="${berry.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src='https://www.pokencyclopedia.info/sprites/items/berries/i_4_0${i}--${berry.name}-berry.png'> <p>${berry.name}</p></div>`;
+                if (berry.id < 9) {
+                    var template = `<div itemid="${berry.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src='https://www.pokencyclopedia.info/sprites/items/berries/i_4_0${berry.id}--${berry.name}-berry.png'> <p>${berry.name}</p></div>`;
                     $('#lista-berrys').append(template);
                 }
     
-                if (i >= 10) {
-                    var template = `<div itemid="${berry.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src='https://www.pokencyclopedia.info/sprites/items/berries/i_4_${i}--${berry.name}-berry.png'> <p>${berry.name}</p></div>`;
+                if (berry.id >= 10) {
+                    var template = `<div itemid="${berry.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src='https://www.pokencyclopedia.info/sprites/items/berries/i_4_${berry.id}--${berry.name}-berry.png'> <p>${berry.name}</p></div>`;
                     $('#lista-berrys').append(template);
                 }
     
-                i++;
+                
             } catch (error) {
                 console.log('Error al obtener la información de la baya:', error);
             }
         }
+        for (var i = 1; i < totalBerries / 20; i++) {
+            var template = `<li class="page-item item" idPag="${i}"><spam class="page-link" href="#">${i}</spam></li>`;
+            $('#pagination').append(template);
+        };
     });
+
+    $(document).on('click', '.item', function () {
+        var pagina = $(this).attr('idPag');
+        gestBerryOffset((pagina - 1) * 20);
+    });
+
+
+    function gestBerryOffset(offset){
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/berry?limit=20&offset=${offset}`,
+            type: 'GET'
+        }).done(async function(respuesta){
+            var listaBerries = respuesta.results;
+            var j = 1;
+            borrar();
+            for (const item of listaBerries) {
+                try {
+                    const berry = await $.ajax({
+                        url: item.url,
+                        type: 'GET'
+                    });
+                    
+                    if (berry.id < 9) {
+                        var template = `<div itemid="${berry.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src='https://www.pokencyclopedia.info/sprites/items/berries/i_4_0${berry.id}--${berry.name}-berry.png'> <p>${berry.name}</p></div>`;
+                        $('#lista-berrys').append(template);
+                    }
+        
+                    if (berry.id >= 10) {
+                        var template = `<div itemid="${berry.id}" class="btn btn-detail badge rounded-pill col-2 mb-4 ms-5 mt-5"><img src='https://www.pokencyclopedia.info/sprites/items/berries/i_4_${berry.id}--${berry.name}-berry.png'> <p>${berry.name}</p></div>`;
+                        $('#lista-berrys').append(template);
+                    }
+        
+                    i++;
+                } catch (error) {
+                    console.log('Error al obtener la información de la baya:', error);
+                }
+            }
+        })
+
+    }
 
         /*function url (nombre){
             var img='';
@@ -46,7 +90,9 @@ $(document).ready(function(){
 
     });
 
+    function borrar() {
+        $("#lista-berrys").empty();
+    }
     
         
     
-});
